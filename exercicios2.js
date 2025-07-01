15afc7d257665c15caf42d5bab73cb82a961f8d0
Gabriel Trevisan Ribeiro <162700611+gTrevisanz@users.noreply.github.com>
2025-07-01 08:38:44 -0300

    Update exercicios2.js

--- exercicios2.js
+++ exercicios2.js
@@ -1,18 +1,1 @@
-let separarParimpar = (nums) => {
-    let pares = []
-    let impares = []
-    let indicePar = 0
-    let indiceImpar = 0
 
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] % 2 === 0) {
-            pares[indicePar] = nums[i]
-            indicePar++
-        } else {
-            impares[indiceImpar] = nums[i]
-            indiceImpar++
-        }
-    }
-
-    return `números pares: ${pares} e os números ímpares são: ${impares}`
-}
